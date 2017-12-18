e8781c184823ec272a9a5ec30e7d63825f3c572a
Piet Hoffmann <hoffmannpiet42@gmail.com>
2017-12-18 13:05:06 +0100

    uni1

--- plot.py
+++ plot.py
@@ -13,23 +13,27 @@ data = np.genfromtxt("content/var_temp.txt", unpack=True)
 
 #Apperaturkonstante
 K = 1.1738*10**(-8)
+Kerr = 
 #Dichte der Kugel
 pk = 2413.16
+pkerr = 0.49
 #Dichte der Flüssigkeit
 pf = 988
 
 n = np.zeros(int(data[0].size)*2)
+nerr = np.zeros(int(data[0].size)*2)
 T = np.zeros(int(data[0].size)*2)
 T = np.append(data[2], data[2])
 n = np.append(data[0], data[1])
 
 n *= K*(pk-pf)
+nerr = n*np.sqrt((Kerr*(pk-pf)**2 + (pkerr*K)**2)
 T += 273.15
 
 def f(T, A, B):
     return A*np.exp(B/T)
 
-params, covar = curve_fit(f, T, n, p0=(0, 1.14))
+params, covar = curve_fit(f, T, n, absolute_sigma=True, sigma=nerr, p0=(0, 1.14))
 uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
 print("Parameter A und B: ")
 print(uparams)
@@ -38,7 +42,7 @@ print(uparams)
 #print(1/T)
 #print(n)
 
-plt.plot(1/T, n, ".", label="Messwerte")
+plt.plot(1/T, n, yerr=nerr, ".", label="Messwerte")
 plt.plot(1/T, f(T, *params), label="Regression")
 plt.yscale("log")
 plt.ylabel(r"$\eta/\si{\pascal\second}$")
